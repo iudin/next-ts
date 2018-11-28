fc5ea616fa72bee56886513921de56f879130d22
Fedor Iudin <fd.iudin@gmail.com>
2018-11-28 22:56:47 +0300

    Настроить snapshot-тесты для styled-components

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -7,10 +7,10 @@ const Title = styled.h1`
 
 export default class extends Component<{ postId: string }> {
   static getInitialProps({ query: { id } }) {
+    console.log('C', id);
     return { postId: id };
   }
   render() {
-    console.log('C', this.props.postId);
     return (
       <div>
         <Title>My blog post #{this.props.postId}</Title>
